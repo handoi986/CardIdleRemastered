53e8e3a11b8216c17ce5e27963cde8f5347a8dde
aleksandr.sharykin <ash.sendbox@gmail.com>
2024-03-17 16:27:29 +0400

    new version

--- SourceCode/CardIdleRemastered/Properties/AssemblyInfo.cs
+++ SourceCode/CardIdleRemastered/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CardIdleRemastered")]
-[assembly: AssemblyCopyright("Copyright © ASh 2018")]
+[assembly: AssemblyCopyright("Copyright © ASh 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -40,16 +40,5 @@ using System.Windows;
     // app, or any theme specific resource dictionaries)
 )]
 
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.4.0")]
-[assembly: AssemblyFileVersion("2.5.4.0")]
+[assembly: AssemblyVersion("2.24.3.14")]
+[assembly: AssemblyFileVersion("2.24.3.14")]
